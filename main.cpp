95fbc27a6e546502174ff960a97520c5fd59e492
annekgomes <162642489+annekgomes@users.noreply.github.com>
2024-04-05 20:22:59 -0400

    idade e altura em c++
    Idade e altura em c++

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+    int main () {
+        int idade;
+        float altura;
+
+        cin >> idade;
+        cin >> altura;
+
+        cout << "Sua idade eh: " << idade << endl;
+        cout << "Sua altura eh: " << altura << endl;
+        return 0;
+}
